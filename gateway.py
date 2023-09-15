9c278ca2e15764a54cb031c8536e272d5e7b1b94
Prometeus <evgeniy.jeka@gmail.com>
2023-09-15 12:19:29 +0300

    Gateway - in progress

--- gateway.py
+++ gateway.py
@@ -14,25 +14,56 @@ except ModuleNotFoundError:
 logging.basicConfig(level=logging.INFO)
 
 
-class Gateway:
+# class Gateway:
 
-    def __init__(self):
-        self.app = Flask(__name__)
+    # def __init__(self):
+    #     self.app = Flask(__name__)
 
-    def run(self):
+# Initiating API Server
+app = Flask(__name__)
 
-        @self.app.route("/user/receive_prompt", methods=['POST'])
-        def receive_user_prompt():
-            request_content = request.get_json()
+def extract_user_data(jwt_token):
 
-            try:
-                user_data = request_content['user_data']  # User name & token as JWT
-                user_prompt = request_content['user_prompt']
+    # Temporary stub
+    # Parsing JWT
 
-            except KeyError:
-                return {"error": "Invalid Log In request"}
+    user_name = "Lisa"
+    user_token = "xxx"
 
-            # Decode user name
-            # Decode user token
-            # Validate token (T.B.D.) - > If validated forward user prompt - > return model output.
+    return user_name, user_token
+
+def user_token_validator(token_to_validate):
+    # Authotization method - temporary stub
+    return True
+
+
+@app.route("/receive_prompt", methods=['POST'])
+def receive_user_prompt():
+    request_content = request.get_json()
+
+    try:
+        user_data = request_content['user_data']  # User name & token as JWT
+        user_prompt = request_content['user_prompt']
+
+    except KeyError:
+        return {"error": "Invalid Log In request"}
+
+    # Getting user name and user token - the former is used to verify the current user is authorized
+    user_name, user_token = extract_user_data(user_data)
+    token_validation = user_token_validator(user_token)
+
+    if token_validation is False:
+        logging.warning(f"Gateway: authorization issue - user {user_name} tried to use"
+                        f" an invalid access token {user_token}")
+        return {"error": "Access denied"}
+
+    logging.info(f"Gateway: Forwarding to the model {user_name} : {user_prompt}")
+    # return {"status": "ok"}
+
+    # Forwarding user name and user prompt to the model
+    return MiddleLayer.handle_user_prompt(user_name, user_prompt)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
 
